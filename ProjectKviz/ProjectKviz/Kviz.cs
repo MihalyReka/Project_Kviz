305991fe067766bfe3fd384fc6dc9c3faef4e76d
MihalyReka <mihajreka@gmail.com>
2022-02-25 22:53:47 +0100

    Téma kiválasztása

---
+++ ProjectKviz/ProjectKviz/Kviz.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectKviz
+{
+    class Kviz
+    {
+        string tantargy;
+        string tema;
+        string kerdes;
+        string valaszA;
+        string valaszB;
+        string valaszC;
+        string valaszD;
+        string helyes;
+        public Kviz(string sor)
+        {
+
+            var sorE = sor.Split(';');
+            Tantargy = sorE[0];
+            Tema= sorE[1];
+            Kerdes= sorE[2];
+            valaszA = sorE[3];
+            valaszB = sorE[4];
+            valaszC = sorE[5];
+            valaszD = sorE[6];
+            Helyes=sorE[7];
+        }
+        public string Tantargy { get => tantargy; private set => tantargy = value; }
+        public string Tema{ get => tema; private set => tema = value; }
+        public string Kerdes { get => kerdes; private set => kerdes = value; }
+        public string ValaszA { get => valaszA; private set => valaszA = value; }
+        public string ValaszB { get => valaszB; private set => valaszB = value; }
+        public string ValaszC { get => valaszC; private set => valaszC = value; }
+        public string ValaszD { get => valaszD; private set => valaszD = value; }
+
+        public string Helyes { get => helyes; private set => helyes = value; }
+    }
+}
--- ProjectKviz/ProjectKviz/MainWindow.xaml.cs
+++ ProjectKviz/ProjectKviz/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,10 +21,82 @@ namespace ProjectKviz
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Kviz> kviz;
+        List<string> tantargyak = new List<string>() { "töri", "matek" };
+
         public MainWindow()
         {
             InitializeComponent();
+            FajlBeolvasasa("KvizhezSzoveg.txt");
         }
 
+        private void FajlBeolvasasa(string fajlNeve)
+        {
+            kviz = new List<Kviz>();
+            foreach (var s in File.ReadAllLines(fajlNeve).Skip(1))
+            {
+                kviz.Add(new Kviz(s));
+            }
+            
+           
+            tantargyCB.ItemsSource = tantargyak;
+
+        }
+
+        private void tantargyCB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+            string tantargy = tantargyCB.SelectedItem.ToString();
+            
+            List<string> tema = new List<string>();
+
+            foreach (var v in kviz)
+            {
+                tema.Add(v.Tema);
+            }
+            List<string> toriTemak = new List<string>() { "vegyes","dátum" };
+            List<string> matekTemak = new List<string>() { "számelmélet", "fogalmak" };
+         
+            
+
+            if (tantargy=="töri")
+            {
+                temakorCB.ItemsSource = toriTemak;
+            }
+            else if (tantargy=="matek")
+            {
+                temakorCB.ItemsSource = matekTemak;    
+            }
+
+        }
+
+        private void TemakorCB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string temakor = temakorCB.SelectedItem.ToString();
+            int i = 0;
+            while (i < kviz.Count && kviz[i].Tema != temakor)
+                i++;
+            kerdes.Content = kviz[i].Kerdes;
+            valaszA.Content = kviz[i].ValaszA;
+            valaszC.Content = kviz[i].ValaszC;
+            valaszB.Content = kviz[i].ValaszB;
+            valaszD.Content = kviz[i].ValaszD;
+
+        }
+
+        private void tovabbGomb_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void visszaGomb_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void kiertekeles_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
